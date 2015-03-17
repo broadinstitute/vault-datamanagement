673b22ddfa84d39ecf473dab122dcc6b2bf2c488
David An <davidan@broadinstitute.org>
2015-03-17 12:42:37 -0400

    fix typo in 'fileType' constant

--- src/main/scala/org/broadinstitute/dsde/vault/datamanagement/domain/RelationKeyValue.scala
+++ src/main/scala/org/broadinstitute/dsde/vault/datamanagement/domain/RelationKeyValue.scala
@@ -3,6 +3,6 @@ package org.broadinstitute.dsde.vault.datamanagement.domain
 case class RelationKeyValue(entityType: String, attributeName: String)
 
 object RelationKeyValue {
-  val FILE_TYPE = RelationKeyValue("filyType", "name")
+  val FILE_TYPE = RelationKeyValue("fileType", "name")
   val FILE_PATH = RelationKeyValue("file", "path")
 }
